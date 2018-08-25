35c5c2cbec9efb7aea42d38442984f11eb72d61a
Lucas Duarte <lucasduarte1112@outlook.com>
2018-08-25 12:22:06 -0300

    v0.1

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Search, User } from './styles';
+
+const Header = () => (
+  <Container>
+    <Search>
+      <input type="" id="" name="" placeholder="Search" />
+    </Search>
+
+    <User>
+      <img src="https://avatars1.githubusercontent.com/u/22087517?v=4" alt="Avatar" />
+      Lucas Duarte
+    </User>
+  </Container>
+);
+
+export default Header;
